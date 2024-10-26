b7c4b70bb206e059e3943b840b41fadf36e53e6e
exchyphen <exc.hyphen@gmail.com>
2024-10-25 18:28:24 -0700

    renamed from xiaolaw to daphnexiaoesq

---
+++ src/components/ContactForm.jsx
@@ -0,0 +1,10 @@
+import "./cell.css";
+
+const ContactForm = (props) => {
+  /* helper functions */
+  function isEmail(input) {
+    return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(input);
+  }
+};
+
+export default ContactForm;
