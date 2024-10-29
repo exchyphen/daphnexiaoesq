fceb07b26562e807ec3cf47174a4f76bd68058ab
exchyphen <exc.hyphen@gmail.com>
2024-10-28 22:58:34 -0700

    contact form

--- src/routes/contact.jsx
+++ src/routes/contact.jsx
@@ -8,29 +8,72 @@ import "./contact.css";
 const Contact = () => {
   // states
   const [email, setEmail] = useState("");
+  const [emailError, setEmailError] = useState(false);
+
+  const [name, setName] = useState("");
+  const [nameError, setNameError] = useState(false);
+
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [phoneNumberError, setPhoneNumberError] = useState(false);
+
+  const [message, setMessage] = useState("");
+  const [messageError, setMessageError] = useState(false);
 
   /* helper functions */
   function isEmail(input) {
     return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(input);
   }
 
+  function isPhoneNumber(input) {
+    if (input.length !== 10) {
+      return false;
+    }
+
+    return !isNaN(input);
+  }
+
   // send data
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log("submitting email", email);
+
+    // validation
+    if (name.length === 0) {
+      setNameError(true);
+      return;
+    }
+    setNameError(false);
+
+    if (!isEmail(email)) {
+      setEmailError(true);
+      return;
+    }
+    setEmailError(false);
+
+    if (!isPhoneNumber(phoneNumber)) {
+      setPhoneNumberError(true);
+      return;
+    }
+    setPhoneNumberError(false);
+
+    if (message.length === 0) {
+      setMessageError(true);
+      return;
+    }
+    setMessageError(false);
 
     const formData = {
+      name: name,
       email: email,
+      phoneNumber: phoneNumber,
+      message: message,
     };
 
     axios
       .post("https://daphnexiaoesq.com//processContact.php", formData)
       .then((response) => {
         console.log(response);
-        console.log("success");
       })
       .catch((error) => {
-        console.log("error");
         console.log(error);
       });
   };
@@ -44,13 +87,72 @@ const Contact = () => {
       <div className="content__main">
         <Sidebar></Sidebar>
         <section className="text__main--section">
-          <form onSubmit={handleSubmit}>
-            <input
-              type="email"
-              onChange={(e) => setEmail(e.target.value)}
-              required
-            ></input>
-            <button type="submit">Submit</button>
+          <form className="contact__form" onSubmit={handleSubmit}>
+            <div>
+              <label className={nameError ? "label--error" : ""}>Name</label>
+              <input
+                className={
+                  "contact__input" + (nameError ? " contact__input--error" : "")
+                }
+                type="text"
+                onChange={(e) => setName(e.target.value)}
+              ></input>
+              {nameError ? (
+                <p className="contact__error">Please enter your name.</p>
+              ) : null}
+            </div>
+            <div>
+              <label className={emailError ? "label--error" : ""}>Email</label>
+              <input
+                className={
+                  "contact__input" +
+                  (emailError ? " contact__input--error" : "")
+                }
+                type="email"
+                onChange={(e) => setEmail(e.target.value)}
+              ></input>
+              {emailError ? (
+                <p className="contact__error">Please enter a valid email.</p>
+              ) : null}
+            </div>
+            <div>
+              <label className={phoneNumberError ? "label--error" : ""}>
+                Phone
+              </label>
+              <input
+                className={
+                  "contact__input" +
+                  (phoneNumberError ? " contact__input--error" : "")
+                }
+                onChange={(e) => setPhoneNumber(e.target.value)}
+              ></input>
+              {phoneNumberError ? (
+                <p className="contact__error">
+                  Please a valid phone number (only numbers).
+                </p>
+              ) : null}
+            </div>
+            <div>
+              <div className={messageError ? "label--error" : ""}>Message</div>
+              <textarea
+                className={
+                  "contact__input--message" +
+                  (messageError ? " contact__input--error" : "")
+                }
+                type="text"
+                onChange={(e) => setMessage(e.target.value)}
+              ></textarea>
+              {messageError ? (
+                <p className="contact__error">Please provide a message.</p>
+              ) : null}
+            </div>
+            <button
+              className="contact__submit"
+              type="submit"
+              onClick={handleSubmit}
+            >
+              Submit
+            </button>
           </form>
         </section>
       </div>
